2c4b325aace75adc51710428a1b806c90ace9dfd
Muhtadi23 <miranmuhtadi121@gmail.com>
2024-12-20 14:36:57 +0600

    btn color changed

--- src/components/LoginBtn.jsx
+++ src/components/LoginBtn.jsx
@@ -6,15 +6,41 @@ import { FcGoogle } from "react-icons/fc";
 const LoginBtn = () => {
     return (
         <div className="max-w-[400px] mx-auto flex flex-col gap-2 p-4">
-            <Button variant="outlined" className="flex items-center gap-2">
+            <Button variant="outlined" className="flex items-center gap-2"
+                sx={{
+                    color: 'black', // Sets the text color
+                    borderColor: 'black', // Optional: Matches the button border to the text color
+                    '&:hover': {
+                        color: 'blue', // Changes text color on hover
+                        borderColor: 'blue',
+                    },
+                }}
+            >
                 <span className="text-xl"><FcGoogle /></span>
-                <span className="text-black">Continue with Google</span>
+                <span >Continue with Google</span>
             </Button>
-            <Button variant="outlined" className="flex items-center gap-2">
+            <Button variant="outlined" className="flex items-center gap-2"
+                sx={{
+                    color: 'black', // Sets the text color
+                    borderColor: 'black', // Optional: Matches the button border to the text color
+                    '&:hover': {
+                        color: 'blue', // Changes text color on hover
+                        borderColor: 'blue',
+                    },
+                }}
+            >
                 <span className="text-xl"> <BsFacebook /></span>
-                <span className="text-black">Continue with Google</span>
+                <span>Continue with Google</span>
             </Button>
-            <Button variant="outlined" className="flex items-center gap-2">
+            <Button variant="outlined" className="flex items-center gap-2"
+                sx={{
+                    color: 'black', // Sets the text color
+                    borderColor: 'black', // Optional: Matches the button border to the text color
+                    '&:hover': {
+                        color: 'blue', // Changes text color on hover
+                        borderColor: 'blue',
+                    },
+                }}>
                 <span className="text-xl"><FaSquareXTwitter /></span>
                 <span className="text-black">Continue with Google</span>
             </Button>
